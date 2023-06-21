488b4bfe8faabc1941503dabf098ce628fce3cd8
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-21 17:22:03 +0100

    Add op_action typedef to the main header file

--- monty.h
+++ monty.h
@@ -1,6 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -27,7 +31,28 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_no);
 } instruction_t;
+
+typedef void (*op_action)(stack_t **stack, unsigned int line_no);
+
+void op_pchar(stack_t **stack, unsigned int line_no);
+void op_stack(stack_t **stack, unsigned int line_no);
+void op_queue(stack_t **stack, unsigned int line_no);
+void op_push(stack_t **stack, unsigned int line_no);
+void op_pall(stack_t **stack, unsigned int line_no);
+void op_swap(stack_t **stack, unsigned int line_no);
+void op_pint(stack_t **stack, unsigned int line_no);
+void op_pstr(stack_t **stack, unsigned int line_no);
+void op_rotl(stack_t **stack, unsigned int line_no);
+void op_rotr(stack_t **stack, unsigned int line_no);
+void op_pop(stack_t **stack, unsigned int line_no);
+void op_add(stack_t **stack, unsigned int line_no);
+void op_nop(stack_t **stack, unsigned int line_no);
+void op_sub(stack_t **stack, unsigned int line_no);
+void op_div(stack_t **stack, unsigned int line_no);
+void op_mul(stack_t **stack, unsigned int line_no);
+void op_mod(stack_t **stack, unsigned int line_no);
+instruction_t *op_list(void);
 
 #endif /** MONTY_H **/
